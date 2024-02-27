bdfa983340c1dbe272baec39cd6663d77acd0a1a
BillySh <A01657143@tec.mx>
2024-02-26 21:05:56 -0600

    PokeApiIntegrated

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,7 +1,7 @@
 import "./searchBox.css";
 
 interface SearchBoxProps {
-  color?: stirng;
+  color?: string;
   label: string;
   onClick: () => void;
 }
@@ -87,7 +87,6 @@ export default function SearchBox(props: SearchBoxProps) {
             id="search-dropdown"
             className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
             placeholder="Search Mockups, Logos, Design Templates..."
-            required=""
           />
           <button
             type="submit"
